ec8349f00c5ff6289943d0cb1abe498f9b4a3d3f
lain <30793687+liminalitythree@users.noreply.github.com>
2020-08-01 08:59:26 -0600

    run rustfmt

--- src/error.rs
+++ src/error.rs
@@ -1,31 +1,16 @@
-use twilight_http::{
-    error::Error as TwilightError,
-    routing::PathParseError,
-};
 use http::{Error as HttpError, Uri};
 use hyper::Error as HyperError;
 use reqwest::Error as ReqwestError;
 use snafu::Snafu;
+use twilight_http::{error::Error as TwilightError, routing::PathParseError};
 
 #[derive(Debug, Snafu)]
 #[snafu(visibility(pub(crate)))]
 pub enum RequestError {
-    ChunkingRequest {
-        source: HyperError,
-    },
-    ChunkingResponse {
-        source: ReqwestError,
-    },
-    InvalidPath {
-        source: PathParseError,
-    },
-    MakingResponseBody {
-        source: HttpError,
-    },
-    NoPath {
-        uri: Uri,
-    },
-    RequestIssue {
-        source: TwilightError,
-    },
+    ChunkingRequest { source: HyperError },
+    ChunkingResponse { source: ReqwestError },
+    InvalidPath { source: PathParseError },
+    MakingResponseBody { source: HttpError },
+    NoPath { uri: Uri },
+    RequestIssue { source: TwilightError },
 }
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,14 @@
 mod error;
 
-use twilight_http::{
-    client::Client,
-    request::Request as TwilightRequest,
-    routing::Path,
-};
 use error::{
-    ChunkingRequest,
-    ChunkingResponse,
-    InvalidPath,
-    MakingResponseBody,
-    RequestError,
-    RequestIssue,
+    ChunkingRequest, ChunkingResponse, InvalidPath, MakingResponseBody, RequestError, RequestIssue,
 };
 use http::request::Parts;
 use hyper::{
     body::Body,
-    server::{
-        conn::AddrStream,
-        Server,
-    },
-    service,
-    Request,
-    Response,
+    server::{conn::AddrStream, Server},
+    service, Request, Response,
 };
-use tracing::{debug, error, info};
-use tracing_subscriber::{fmt, EnvFilter};
-use tracing_subscriber::prelude::*;
-use tracing_log::LogTracer;
 use snafu::ResultExt;
 use std::{
     convert::TryFrom,
@@ -36,13 +17,18 @@ use std::{
     net::{IpAddr, SocketAddr},
     str::FromStr,
 };
+use tracing::{debug, error, info};
+use tracing_log::LogTracer;
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::{fmt, EnvFilter};
+use twilight_http::{client::Client, request::Request as TwilightRequest, routing::Path};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     LogTracer::init()?;
 
-    let log_filter_layer = EnvFilter::try_from_default_env()
-        .or_else(|_| EnvFilter::try_new("info"))?;
+    let log_filter_layer =
+        EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new("info"))?;
     let log_fmt_layer = fmt::layer();
 
     let log_subscriber = tracing_subscriber::registry()
@@ -72,8 +58,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
     });
 
-    let server = Server::bind(&address)
-        .serve(service);
+    let server = Server::bind(&address).serve(service);
 
     info!("Listening on http://{}", address);
 
@@ -84,33 +69,38 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-async fn handle_request(client: Client, request: Request<Body>) -> Result<Response<Body>, RequestError> {
+async fn handle_request(
+    client: Client,
+    request: Request<Body>,
+) -> Result<Response<Body>, RequestError> {
     debug!("Incoming request: {:?}", request);
 
     let (parts, body) = request.into_parts();
-    let Parts { method, uri, headers, .. } = parts;
+    let Parts {
+        method,
+        uri,
+        headers,
+        ..
+    } = parts;
 
     let trimmed_path = if uri.path().starts_with("/api/v6") {
         uri.path().replace("/api/v6", "")
     } else {
         uri.path().to_owned()
     };
-    let path = Path::try_from((
-        method.clone(),
-        trimmed_path.as_ref(),
-    )).context(InvalidPath)?;
+    let path = Path::try_from((method.clone(), trimmed_path.as_ref())).context(InvalidPath)?;
 
-    let bytes = (hyper::body::to_bytes(body).await.context(ChunkingRequest)?).to_owned().to_vec();
+    let bytes = (hyper::body::to_bytes(body).await.context(ChunkingRequest)?)
+        .to_owned()
+        .to_vec();
 
     let path_and_query = match uri.path_and_query() {
         Some(v) => v.as_str().replace("/api/v6/", "").into(),
         None => {
             debug!("No path in URI: {:?}", uri);
 
-            return Err(RequestError::NoPath {
-                uri,
-            });
-        },
+            return Err(RequestError::NoPath { uri });
+        }
     };
     let raw_request = TwilightRequest {
         body: Some(bytes),
@@ -128,14 +118,15 @@ async fn handle_request(client: Client, request: Request<Body>) -> Result<Respon
 
     let bytes = resp.bytes().await.context(ChunkingResponse)?;
 
-    let mut builder = Response::builder()
-        .status(status);
+    let mut builder = Response::builder().status(status);
 
     if let Some(headers) = builder.headers_mut() {
         headers.extend(resp_headers);
     }
 
-    let resp = builder.body(Body::from(bytes)).context(MakingResponseBody)?;
+    let resp = builder
+        .body(Body::from(bytes))
+        .context(MakingResponseBody)?;
 
     debug!("Response: {:?}", resp);
 
