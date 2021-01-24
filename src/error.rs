3e50bae7f5ccd685210c485d3b05815359bc5f4c
Jens Reidel <adrian@travitia.xyz>
2021-01-25 00:35:57 +0100

    chore: update to twilight 0.3 and hyper 0.14 (#7)
    Update `twilight-http` from 0.2 to 0.3 and `hyper` to 0.14.
    
    Approved-by: AEnterprise <aenterprise@aenterprise.info>
    Merged-by: Vivian Hellyer <vivian@hellyer.dev>
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,5 @@
 use http::{Error as HttpError, Uri};
 use hyper::Error as HyperError;
-use reqwest::Error as ReqwestError;
 use snafu::Snafu;
 use twilight_http::{error::Error as TwilightError, routing::PathParseError};
 
@@ -8,7 +7,6 @@ use twilight_http::{error::Error as TwilightError, routing::PathParseError};
 #[snafu(visibility(pub(crate)))]
 pub enum RequestError {
     ChunkingRequest { source: HyperError },
-    ChunkingResponse { source: ReqwestError },
     InvalidPath { source: PathParseError },
     MakingResponseBody { source: HttpError },
     NoPath { uri: Uri },
