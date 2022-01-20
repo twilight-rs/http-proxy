703b2f0651f1ab208893dd215686191c34b1a156
Jens Reidel <adrian@travitia.xyz>
2022-01-20 14:28:57 +0100

    feat: improve error handling (#47)
    return a 5xx response from the proxy depending on the given error.
    
    Closes #26 
    
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,17 @@
-use http::{uri::InvalidUri, Method};
-use hyper::Error as HyperError;
+use http::{uri::InvalidUri, Method, Response};
+use hyper::{Body, Error as HyperError};
 use std::{
     error::Error,
     fmt::{Display, Formatter, Result as FmtResult},
 };
 use twilight_http_ratelimiting::request::PathParseError;
+
+static ACQUIRING_TICKET_FAILED_MSG: &str =
+    "http-proxy: Acquiring ticket from the ratelimiter failed";
+static INVALID_URI_MSG: &str = "http-proxy: Failed to create URI for requesting Discord API";
+static INVALID_METHOD_MSG: &str = "http-proxy: Unsupported HTTP method in request";
+static INVALID_PATH_MSG: &str = "http-proxy: Failed to parse API path from client request";
+static REQUEST_ISSUE_MSG: &str = "http-proxy: Error requesting the Discord API";
 
 #[allow(clippy::module_name_repetitions)]
 #[derive(Debug)]
@@ -24,6 +31,23 @@ pub enum RequestError {
     RequestIssue {
         source: HyperError,
     },
+}
+
+impl RequestError {
+    pub fn as_response(&self) -> Response<Body> {
+        let (status_code, body) = match self {
+            RequestError::AcquiringTicket { .. } => (500, ACQUIRING_TICKET_FAILED_MSG),
+            RequestError::InvalidURI { .. } => (500, INVALID_URI_MSG),
+            RequestError::InvalidMethod { .. } => (501, INVALID_METHOD_MSG),
+            RequestError::InvalidPath { .. } => (501, INVALID_PATH_MSG),
+            RequestError::RequestIssue { .. } => (502, REQUEST_ISSUE_MSG),
+        };
+
+        Response::builder()
+            .status(status_code)
+            .body(Body::from(body))
+            .unwrap()
+    }
 }
 
 impl Display for RequestError {
